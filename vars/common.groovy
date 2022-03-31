b06a81d9bf61269945e8ad62347e3b40501d8dac
bartoszrekowski <102472859+bartoszrekowski@users.noreply.github.com>
2022-03-31 17:59:44 +0200

    Update common.groovy
    frsedf

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,7 +1,11 @@
 def info(message) {
-    echo "INFO: ${message}"
+    echo "INFORMATION: ${message}"
 }
 
 def warning(message) {
     echo "WARNING: ${message}"
+}
+
+def error(message) {
+    echo "ERROR: ${message}"
 }
